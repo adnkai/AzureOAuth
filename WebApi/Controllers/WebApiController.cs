d5d362ee16f1150d01c270de5b54cc38f738fde8
Kai Roth <kai.roth@adn.de>
2020-09-07 11:58:55 +0200

    base structure

---
+++ WebApi/Controllers/WebApiController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WebApiController : ControllerBase
+    {
+        // GET api/WebApi  
+        public IEnumerable<string> Get()  
+        {  
+            return new string[] { "Hello REST API", "I am Authorized" };  
+        }  
+  
+        // GET api/WebApi/5  
+        public string Get(int id)  
+        {  
+            return "Hello Authorized API with ID = " + id;  
+        }  
+  
+        // POST api/WebApi  
+        public void Post([FromBody]string value)  
+        {  
+        }  
+  
+        // PUT api/WebApi/5  
+        public void Put(int id, [FromBody]string value)  
+        {  
+        }  
+  
+        // DELETE api/WebApi/5  
+        public void Delete(int id)  
+        {  
+        }  
+    }
+}
